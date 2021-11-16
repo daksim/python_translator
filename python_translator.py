07d884740a0dc3509d9c12783e20aeb37d23050b
daksim <daksimzhang@gmail.com>
2021-11-16 19:46:32 +0800

    apply baidu translator

--- python_translator.py
+++ python_translator.py
@@ -3,17 +3,15 @@ from tkinter.constants import LEFT, RIGHT
 import win32clipboard as w
 import win32con
 import json
+import requests
+import random
+from hashlib import md5
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.tmt.v20180321 import tmt_client, models
 
-'''
-Fill the key
-'''
-SecretId = ""
-SecretKey = ""
 
 # Functions to read or change texts in windows clipboard
 def getText():
@@ -28,8 +26,10 @@ def setText(aString):
     w.SetClipboardData(win32con.CF_TEXT, aString)
     w.CloseClipboard()
 
-# Translate API (en-us -> zh-cn)
+# Tencent translate API (en-us -> zh-cn)
 def translate_tencent(text):
+    SecretId = "INPUT_YOUR_APPID"
+    SecretKey = "INPUT_YOUR_APPKEY"
     try:
         cred = credential.Credential(SecretId, SecretKey)
         httpProfile = HttpProfile()
@@ -54,6 +54,43 @@ def translate_tencent(text):
     except TencentCloudSDKException as err:
         return err
 
+# Baidu translate API (en-us -> zh-cn)
+def translate_baidu(text):
+    # Set your own appid/appkey.
+    appid = 'INPUT_YOUR_APPID'
+    appkey = 'INPUT_YOUR_APPKEY'
+
+    # For list of language codes, please refer to `https://api.fanyi.baidu.com/doc/21`
+    from_lang = 'en'
+    to_lang =  'zh'
+
+    endpoint = 'http://api.fanyi.baidu.com'
+    path = '/api/trans/vip/translate'
+    url = endpoint + path
+
+    query = text
+
+    # Generate salt and sign
+    def make_md5(s, encoding='utf-8'):
+        return md5(s.encode(encoding)).hexdigest()
+
+    salt = random.randint(32768, 65536)
+    sign = make_md5(appid + query + str(salt) + appkey)
+
+    # Build request
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    payload = {'appid': appid, 'q': query, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
+
+    # Send request
+    r = requests.post(url, params=payload, headers=headers)
+    result = r.json()
+
+    # Show response
+    result_text = result['trans_result'][0]['dst']
+    print(result_text)
+    return result_text
+
+
 
 
 if __name__ == '__main__':
@@ -74,7 +111,7 @@ if __name__ == '__main__':
     def trans():
         text = getText()
         print(text)
-        text_transed = translate_tencent(text)
+        text_transed = translate_baidu(text)
         var.set(text_transed)
     
     b = tk.Button(frm2, text='go', width=3, height=2, command=trans)
